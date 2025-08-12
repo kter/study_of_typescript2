bb2778771f3e3e5210753619bc65a1995922995a
Takahashi Tomohiko <takahashi@tomohiko.io>
2025-08-12 22:39:06 +0900

    step3

--- book.ts
+++ book.ts
@@ -1,4 +1,4 @@
-class Book {
+export class Book {
 
   title: string;
   author: string;
---
+++ library.ts
@@ -0,0 +1,29 @@
+import { Book } from "./book";
+
+class Library {
+  books: Book[];
+
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(book: Book): void {
+    this.books.push(book);
+  }
+
+  listBooks(): void {
+    this.books.forEach((book) => console.log(book.getInfo()));
+  }
+
+  findByAuthor(author: string): Book[] {
+    return this.books.filter((book) => (book.author === author));
+  }
+}
+
+const library = new Library();
+library.addBook(new Book("坊っちゃん", "夏目漱石", 880));
+library.addBook(new Book("吾輩は猫である", "夏目漱石", 1200));
+library.addBook(new Book("走れメロス", "太宰治", 900));
+
+library.listBooks();
+console.log(library.findByAuthor("夏目漱石").length);

54a1bbc671f51026bda585e405df81df610a4cf4
Takahashi Tomohiko <takahashi@tomohiko.io>
2025-08-12 21:30:03 +0900

    step2

--- book.ts
+++ book.ts
@@ -11,9 +11,14 @@ class Book {
   }
 
   getInfo() {
-    return `タイトル: ${this.title} 著者: ${this.author} 価格: ${this.price}円`;
+    return `タイトル: ${this.title}, 著者: ${this.author}, 価格: ${this.price}円`;
+  }
+
+  getDiscountedPrice(rate: number) {
+    return Math.floor(this.price * (1 - rate));
   }
 }
 
 const book = new Book("坊ちゃん", "夏目漱石", 880);
 console.log(book.getInfo());
+console.log(book.getDiscountedPrice(0.2));

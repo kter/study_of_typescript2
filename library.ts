32953b2ef010e2e278747a50daf240fd5f99a04b
Takahashi Tomohiko <takahashi@tomohiko.io>
2025-08-14 20:55:59 +0900

    step4-3

--- library.ts
+++ library.ts
@@ -1,14 +1,10 @@
 import { Book } from "./book.ts";
 
 class Library {
-  books: Book[];
-
-  constructor() {
-    this.books = [];
-  }
+  books: Book[] = [];
 
   addBook(book: Book): void {
-    if (this.books.some(b => b.title === book.title && b.author === book.author)) {
+    if (this.hasBook(book.title, book.author)) {
       console.warn("タイトルと著者が重複しています");
       throw new Error("Duplicate book");
     }
@@ -21,6 +17,22 @@ class Library {
 
   findByAuthor(author: string): Book[] {
     return this.books.filter(book => (book.author === author));
+  }
+
+  findByTitle(title: string): Book[] {
+    return this.books.filter(book => (book.title === title));
+  }
+
+  hasBook(title: string, author: string): boolean {
+    return this.books.some( b => b.title === title && b.author === author);
+  }
+
+  removeBook(title: string, author: string): boolean {
+    if (!this.hasBook(title, author)) {
+      return false;
+    }
+    this.books = this.books.filter( b => !(b.title === title && b.author === author))
+    return true;
   }
 }
 
@@ -37,3 +49,12 @@ try {
 } catch (e) {
   console.log("重複チェックOK")
 }
+
+console.log(library.hasBook("坊っちゃん", "夏目漱石"));
+console.log(library.hasBook("坊っちゃん2", "夏目漱石"));
+
+library.listBooks();
+console.log(library.removeBook("坊っちゃん", "夏目漱石"));
+library.listBooks();
+console.log(library.removeBook("坊っちゃん", "夏目漱石"));
+library.addBook(new Book("坊っちゃん", "夏目漱石", 880));
